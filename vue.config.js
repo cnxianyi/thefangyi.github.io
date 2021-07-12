7cc93b42b3ae2dd43da6db5edf430fc7d0415350
YIxian <67898014+Thefangyi@users.noreply.github.com>
2021-07-12 19:47:57 +0800

    1.0.0.3
    完成npm run build 输出环境的配置

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+  // 选项...
+  // Default: 'dist' , 此处修改为docs , 以便github读取index.html
+  outputDir: 'docs'
+}
