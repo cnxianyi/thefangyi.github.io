1fba1aa373f09aa70edeca58fb8494cfef81c701
YiXian <67898014+thefangyi@users.noreply.github.com>
2021-10-01 18:14:43 +0800

    1.0.5
    添加检测数据显示功能

--- src/study/english/words.vue
+++ src/study/english/words.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
+		<div class="rank" v-if="ranking">
+			<div class="rank_top">
+				排行榜
+			</div>
+			<div class="rank_content">
+				<div class="line"><p>当日检测次数 : </p><strong>{{rankArray[rankArray.length - 1]}}</strong></div><br>
+				<div class="line"><p>总检测天数 : </p><strong>{{rankArray.length}}</strong></div><br>
+				<div class="line"><p>历史最高检测次数 : </p><strong>{{rankMax}}</strong></div><br>
+				<div class="line"><p>历史最低检测次数 : </p><strong>{{rankMin}}</strong></div><br>
+				<div class="line"><p>每日平均检测次数 : </p><strong>{{rankAverage}}</strong></div><br>
+				
+			</div>
+			<a class="rank_a" @click="getGrass"><p>CLOSE</p></a>
+		</div>
+		<div class="" id="rank_grass">
         <nav-bar>
             <template v-slot:default>
                 English Words
@@ -8,10 +23,12 @@
 				2021年9月21日13:10:56
             </template>
         </nav-bar>
+
 	<div class="xy">
 		<div class="content">
 			<div class="button">
 				<sidebarR></sidebarR>
+				
 				<button class="btn" v-if="switchCard" @click="switchCard = !switchCard , refWords('studyWords')">ALL</button>
 				<button class="btn study" v-if="!switchCard" @click="switchCard = !switchCard , refWords('allWords')">STUDY</button>
 				<!--  -->
@@ -25,7 +42,7 @@
 				<button class="btn study" v-if="!switchCard" @click="addShowStudy = !addShowStudy">添加单词</button>
 				<button class="btn study" v-if="!switchCard" @click="wordsTest">检测</button>
 				<button class="btn study" v-if="!switchCard" @click="Detect">检查重复</button>
-				
+				<button class="btn study" v-if="!switchCard" @click="getGrass">排行榜</button>
 			</div>
 			
 			<div class="addWords" v-if="addShowAll">
@@ -108,6 +125,7 @@
 			</div>
 		</div>
 		</div>
+		</div>
     </div>
 </template>
 
@@ -130,6 +148,9 @@ export default {
 				wordTest: 0, // 测试计数
 				switchCard: true, // 切换单词显示
 				switchWord:true, // 单词刷新
+				ranking:false,// 排行榜
+				rankArray: [],
+				dailyDateId:0,
 				words: {},
 				addShowAll: false,
 				addShowStudy: false,
@@ -143,6 +164,19 @@ export default {
 				NewWords: [],
 				detectMap: new Map,
 				enterWords: '',
+				
+			}
+		},
+		computed: {
+			rankMax(){
+				return Math.max(...this.rankArray)
+			},
+			rankMin(){
+				return Math.min(...this.rankArray)
+			},
+			rankAverage(){
+				let sum = function(x,y){ return x+y;};
+				return Math.round(this.rankArray.reduce(sum)/this.rankArray.length) 
 			}
 		},
 		mounted() {
@@ -154,7 +188,63 @@ export default {
 				
 			});
 		},
+		created() {
+			let date = new Date()
+			//console.log(this.todayYear+'/'+this.todayMonth+'/'+this.todayDay);
+			axios.get(`http://localhost:3000/dailyData`).then((result) => {
+				this.dailyDateId = result.data[result.data.length - 1].id // 获取今天的ID
+				let b = result.data[result.data.length - 1].dateDay !== date.getDate()
+				if ( b ) { // 获取时间，当有今天的数据时，更新number,没有则添加数据
+					axios({ // 添加日期
+					method: 'post',
+					url: `http://localhost:3000/dailyData`,
+					data: {
+						dateYear: date.getFullYear(),
+						dateMonth: date.getMonth()+1,
+						dateDay: date.getDate(),
+						number: 0
+					}
+					});
+				}else{
+					this.wordTest = result.data[result.data.length - 1].number
+				}
+				//console.log(result.data[result.data.length - 1]);
+			})
+		},
+		updated() { 
+			let id = this.dailyDateId
+			//console.log(id);
+			axios({ // 添加日期 数据更新时更新 number
+					method: 'patch',
+					url: `http://localhost:3000/dailyData/${id}`,
+					data: {
+						number: this.wordTest
+					}
+					});
+		},
 		methods: {
+			getGrass(){
+				if(this.ranking){ // 排行榜数据
+					this.ranking = !this.ranking
+					document.getElementById('rank_grass').className = ''
+					
+				}else{
+					this.rankArray = []
+					//axios.get(`http://localhost:3000/dailyData/${this.dailyDateId}`).then((result) => {
+					axios.get(`http://localhost:3000/dailyData`).then((result) => {
+						console.log(result.data.length);
+						for( let i = 0; i<result.data.length; i++){
+							this.rankArray.push(result.data[i].number)
+						}
+						console.log(this.rankArray);
+					})
+					this.ranking = !this.ranking
+					document.getElementById('rank_grass').className = 'grass'
+					
+				}
+				
+				
+			},
 			addImport(id , boo){
 				let nowUrl = this.nowUrl
 				axios({
@@ -238,12 +328,12 @@ export default {
 				if (this.addWords.length >= 1  & this.addMeaning.length >= 1) {
 					alert(this.addWords + this.addMeaning +'  ' + ' 添加成功')
 					if(this.addMeaning.slice(0,1).codePointAt(0) >=97 && this.addMeaning.slice(0,1).codePointAt(0) <=122){
-						
+						// 判断第一个字符是否为字母，如果不是字母则加上 n·
 					}else{
 						this.addMeaning = 'n·'+this.addMeaning
 						
 					}
-					axios({
+					axios({ // 添加单词
 					method: 'post',
 					url: `http://localhost:3000/${str}`,
 					data: {
@@ -254,15 +344,17 @@ export default {
 					});
 					
 				}
-				this.addWords = ''
+				this.addWords = '' // 将输入框清空
 				this.addMeaning = ''
-				
-				setTimeout(() => {
+				//this.words = {}
+
+				setTimeout(() => { // 微任务判断是否重复
 					axios.get(`http://localhost:3000/${str}`).then((result) => {
 					console.log(result.data);
 					this.words = result.data
 					for (let i = 0; i < this.words.length; i++) {
 						this.detectMap.set( this.words[i].word , i)
+						console.log(this.words[i].word);
 					}
 					if (this.detectMap.size < this.words.length) {
 						let getDelete = confirm('单词重复收录，是否删除')
@@ -270,7 +362,7 @@ export default {
 						if(getDelete){
 							axios.delete(`http://localhost:3000/${str}/${len}`)
 							alert('删除成功')
-							location.reload() 
+							location.reload()  
 						}
 					}
 					document.getElementById('words').focus() // 控制聚焦input
@@ -329,6 +421,81 @@ export default {
 		position: relative;
 		top: 1px;
 		background: none;
+	}
+
+	.rank { // 排行榜
+		position: fixed;
+		z-index: 9999;
+		border-radius: 10px;
+		width: calc(100vw - 50%);
+		min-height: 100px;
+		max-height: 300px;
+		left:0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        margin: auto;
+		background-color: #FFFFFF;
+		box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+		
+		.rank_top{
+			box-sizing: border-box;
+			padding: 18px;
+			width: 100%;
+			text-align: center;
+			letter-spacing: 1px;
+			font-size: 20px;
+			font-weight: 300;
+		}
+		.rank_content {
+			padding: 0px 60px;
+			.line {
+				width: 100%;
+				margin: 4px 0px;
+				padding: 2px 0px;
+				p {
+					float: left;
+				}
+				strong {
+					float: right;
+				}
+
+			}
+		}
+		
+			a.rank_a {
+			position: absolute;
+			user-select:none;
+			bottom: 0px;
+			left: 0;
+			right: 0;
+			text-align: center;
+			color: #000000bd;
+			font-weight:100;
+			font-size: 16px;
+			p {
+				padding: 20px;
+			}
+			}
+			a:visited{
+			font-weight: bold;
+			}
+			a:hover{
+			font-weight: bold;
+			color: #000000;
+			p {
+				background-color: #e4e4e47e;
+				border-bottom-left-radius: 10px;
+				border-bottom-right-radius: 10px;
+			}
+			}
+		
+	}
+	.grass {
+		-webkit-filter: blur(6px); /* Chrome, Opera */
+       -moz-filter: blur(6px);
+        -ms-filter: blur(6px);    
+            filter: blur(6px);
 	}
 		
 </style>
